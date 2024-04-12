2cf5ffa3e66d177fac47c1e4c992b3d5bf09a310
poolgp <145465364+poolgp@users.noreply.github.com>
2024-04-12 20:14:52 +0200

    pol

--- app/Http/Controllers/ProveedorController.php
+++ app/Http/Controllers/ProveedorController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Reserva;
 use App\Models\Proveedor;
+use App\Models\User;
+use App\Models\Usuario;
 use Illuminate\Http\Request;
 use Illuminate\Database\QueryException;
 
@@ -13,7 +16,9 @@ class ProveedorController extends Controller
      */
     public function index()
     {
-        //
+        $reservas = Reserva::with('rider')->get();
+        echo $reservas;
+        // return response()->json($reservas);
     }
 
     /**
@@ -21,13 +26,13 @@ class ProveedorController extends Controller
      */
     public function create(Request $request)
     {
-        $id=$request["id"];
-        $calle=$request["calle"];
-        $numero=$request["numero"];
-        $cp=$request["cp"];
-        $ciudad=$request["ciudad"];
-        $logo=$request["nombreDelArchivoDelLogo"];
-        return view("usuarios.proveedor",compact("id","calle","numero","cp","ciudad","logo"));
+        $id = $request["id"];
+        $calle = $request["calle"];
+        $numero = $request["numero"];
+        $cp = $request["cp"];
+        $ciudad = $request["ciudad"];
+        $logo = $request["nombreDelArchivoDelLogo"];
+        return view("usuarios.proveedor", compact("id", "calle", "numero", "cp", "ciudad", "logo"));
     }
 
     /**
@@ -36,42 +41,39 @@ class ProveedorController extends Controller
     public function store(Request $request)
     {
         //Recuperar los datos del formulario
-        $id=$request->input("Id");
-        $calle=$request->input("Calle");
-        $numero=$request->input("Numero");
-        $cp=$request->input("Cp");
-        $ciudad=$request->input("Ciudad");
-        $logo=$request->input("Logo");
-        $latitud=$request->input("Latitud");
-        $longitud=$request->input("Longitud");
+        $id = $request->input("Id");
+        $calle = $request->input("Calle");
+        $numero = $request->input("Numero");
+        $cp = $request->input("Cp");
+        $ciudad = $request->input("Ciudad");
+        $logo = $request->input("Logo");
+        $latitud = $request->input("Latitud");
+        $longitud = $request->input("Longitud");
 
         //Crear un objeto de la clase que representa un registro a la tabla
-        $proveedor=new Proveedor();
+        $proveedor = new Proveedor();
         //Asignar los valores del formulario a su respectivo campo
-        $proveedor->id=$id;
-        $proveedor->calle=$calle;
-        $proveedor->numero=$numero;
-        $proveedor->cp=$cp;
-        $proveedor->ciudad=$ciudad;
-        $proveedor->logo=$logo;
-        $proveedor->lt=$latitud;
-        $proveedor->lng=$longitud;
+        $proveedor->id = $id;
+        $proveedor->calle = $calle;
+        $proveedor->numero = $numero;
+        $proveedor->cp = $cp;
+        $proveedor->ciudad = $ciudad;
+        $proveedor->logo = $logo;
+        $proveedor->lt = $latitud;
+        $proveedor->lng = $longitud;
 
 
-        try
-        {
+        try {
             //Hacer el insert en la tabla
             $proveedor->save();
-            $request->session()->flash("mensaje","Usuario inscrito correctamente.");
-            $response=redirect("/login");
+            $request->session()->flash("mensaje", "Usuario inscrito correctamente.");
+            $response = redirect("/login");
+        } catch (QueryException $ex) {
+            $mensaje = Utilidad::errorMessage($ex);
+            $request->session()->flash("error", $mensaje);
+            $response = redirect("/login");
         }
-        catch(QueryException $ex)
-        {
-            $mensaje=Utilidad::errorMessage($ex);
-            $request->session()->flash("error",$mensaje);
-            $response=redirect("/login");
-        }
-        
+
         return $response;
     }
 
@@ -86,9 +88,9 @@ class ProveedorController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Proveedor $proveedore)
+    public function edit(Proveedor $proveedore, Usuario $user)
     {
-        return view("proveedor/formProveedor",compact("proveedore"));
+        return view("proveedor/formProveedor", compact("proveedore", "user"));
     }
 
     /**
