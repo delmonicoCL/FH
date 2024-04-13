63fc7b89740bc930768c32e84866f313a985179e
oscarArriaza <arriaza22598@vk.com>
2024-04-13 19:59:28 +0200

    34

--- app/Http/Controllers/ReservaController.php
+++ app/Http/Controllers/ReservaController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Reserva;
+use App\Clases\Utilidad;
+use Illuminate\Database\QueryException;
 
 class ReservaController extends Controller
 {
