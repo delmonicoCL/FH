0bfa9c98dda39dca70d8bfefc5d89e5ab5f2db8a
oscarArriaza <arriaza22598@vk.com>
2024-04-17 20:22:21 +0200

    oscar

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,41 +7,73 @@
     @extends("layouts.principal")
     @section('contenido')
         @include('partials.mensajes')
-        <div class="offset-lg-3 col-lg-6 mt-5">
-            <div class="card">
-                <div class="card-header text-light" style="background-color: #DA2427;">Iniciar Sesión</div>
-                <div class="card-body">
-                    <form action="{{action([App\Http\Controllers\UsuarioController::class,'login'])}}" method="POST">
-                        @csrf
+        <div class="row mt-5" style="display:flex; align-items: center; justify-content: center;">
+            <!--<div style="display:flex; align-items: center; justify-content: center;">-->
+            <div class="col-sm-6" style="display:flex; justify-content: flex-end">
+                <img src="{{asset('media/img/zanahoria.png')}}" alt="imagen reloj" height="700vh" width="700vw" draggable="false">
+            </div>
+            <div class="col-sm-6" style="display:flex; align-items: center; justify-content: flex-start">
+                <div class="card">
+                    <div class="card-body">
+                        <form action="{{action([App\Http\Controllers\UsuarioController::class,'login'])}}" method="POST">
+                            @csrf
 
-                        <div class="row mb-3">
-                            <label for="correoElectronico" class="col-sm-2 col-from-label">Correo Electronico</label>
-                            <div class="col-sm-10">
-                                <input type="text" class="form-control" id="correoElectronico" name="CorreoElectronico" value="{{old("CorreoElectronico")}}" autofocus>
+                            <div class="row mb-3">
+                                <div class="col-sm-8">
+                                    <p class="h6">
+                                        Bienvenidos a Food Hero
+                                    </p>
+                                    <p class="h1">
+                                        Iniciar Sesión
+                                    </p>
+                                </div>
+                                <div class="col-sm-4">
+                                    <small>
+                                        ¿No tienes cuenta? Crea una 
+                                    </small>
+                                    <small>
+                                        <a href="{{url("/registros/index")}}">
+                                            aqui
+                                        </a>
+                                    </small>
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="row mb-3">
-                            <label for="contrasenia" class="col-sm-2 col-form-label">Contraseña</label>
-                            <div class="col-sm-10">
-                                <input type="password" class="form-control" id="contrasenia" name="Contrasenia" value="{{old('Contrasenia')}}">
+                            <div class="row mb-3">
+                                <label for="correoElectronico" class="col-sm-2 col-from-label" hidden>
+                                    Correo Electronico
+                                </label>
+                                <div class="col-sm-12">
+                                    <input type="text" class="form-control" id="correoElectronico" name="CorreoElectronico" value="{{old("CorreoElectronico")}}" placeholder="Correo Electrónico" autofocus>
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="row">
-                            <div class="col-sm-12 d-flex flex-row-reverse">
-                                <a href="{{url('/')}}" class="btn btn-secondary float-right ms-1"><i class="fa fa-times" aria-hidden="true"></i>Cancelar</a>
-                                <button type="submit" class="btn btn-primary float-right"><i class="fa fa-check" aria-hidden="true"></i>Aceptar</button>
+                            <div class="row mb-3">
+                                <label for="contrasenia" class="col-sm-2 col-form-label" hidden>
+                                    Contraseña
+                                </label>
+                                <div class="col-sm-12">
+                                    <input type="password" class="form-control" id="contrasenia" name="Contrasenia" value="{{old('Contrasenia')}}" placeholder="Contraseña">
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="row" style="text-align: end;">
-                            <p>
-                                ¿No tienes cuenta? Crea una <a href="{{url("/registros/index")}}">aqui</a>
-                            </p>
-                        </div>
+                            <div class="row mb-3">
+                                <div class="col-sm-12">
+                                    <button type="submit" class="btn btn-primary float-right col-sm-12" style="background-color: #00AEEF;">
+                                        <i class="fa fa-check" aria-hidden="true"></i>
+                                        Iniciar Sesión
+                                    </button>
+                                </div>
+                            </div>
 
-                    </form>
+                            {{--<div class="row" style="text-align: end;">
+                                <p>
+                                    ¿No tienes cuenta? Crea una <a href="{{url("/registros/index")}}">aqui</a>
+                                </p>
+                            </div>--}}
+
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
